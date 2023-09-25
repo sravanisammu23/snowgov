4e40b726df9b90bb423914179b4623dc5e978c18
sravanisammu23 <70464115+sravanisammu23@users.noreply.github.com>
2023-09-25 16:07:13 +0530

    Update tst_modified.py

--- tst_modified.py
+++ tst_modified.py
@@ -13,7 +13,7 @@ from utils import sql as sql
 from PIL import Image
 import base64
 import plotly.express as px
-image = Image.open('snowgov/image.png')
+#image = Image.open('snowgov/image.png')
 st.sidebar.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 #snowflake_config = st.secrets["sf_usage_app"]
 #connect to snowflake function
