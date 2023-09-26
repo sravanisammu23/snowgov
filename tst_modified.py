ac818d9ace88ad7d119e3f8e7a5ec42d39c9d9df
sravanisammu23 <70464115+sravanisammu23@users.noreply.github.com>
2023-09-26 17:02:06 +0530

    Update tst_modified.py

--- tst_modified.py
+++ tst_modified.py
@@ -17,23 +17,7 @@ image = Image.open('image.png')
 st.sidebar.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 #snowflake_config = st.secrets["sf_usage_app"]
 #connect to snowflake function
-SNOWFLAKE_CONFIG = {
 
-    "account": "xh84085.ap-southeast-1",
-
-    "user": "sravani12",
-
-    "password": "Sravani@12",
-
-    "role": "accountadmin",
-
-    "warehouse": "COMPUTE_WH",
-
-    "database": "UTIL_DB",
-
-    "schema": "ADMIN_TOOLS"
-
-}
 if "grant_users" not in st.session_state:
     st.session_state.grant_users = []
 def apply_css_styles():
