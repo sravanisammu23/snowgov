1e49441881986451c5514c1ee7e46cee459e5129
sravanisammu23 <70464115+sravanisammu23@users.noreply.github.com>
2023-09-26 10:40:24 +0530

    Update tst_modified.py

--- tst_modified.py
+++ tst_modified.py
@@ -15,7 +15,7 @@ import base64
 import plotly.express as px
 image = Image.open('image.png')
 st.sidebar.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-#snowflake_config = st.secrets["sf_usage_app"]
+snowflake_config = st.secrets["sf_usage_app"]
 #connect to snowflake function
 SNOWFLAKE_CONFIG = {
     "account": "ie30988.ap-southeast-1",
