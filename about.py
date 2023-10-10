e84dcba854f090fad2e22f6c7297caa9796ccbeb
sravanisammu23 <70464115+sravanisammu23@users.noreply.github.com>
2023-10-10 17:22:03 +0530

    Update about.py

--- about.py
+++ about.py
@@ -981,6 +981,10 @@ def monitor3(tab_id):
         environments = ['All', 'DEV_', 'PROD', 'STAG', 'TEST']
 
         selected_environments = st.multiselect('ENVIRONMENT :', environments, default=['All'])
+	if not selected_environments:
+        st.warning("Please select at least one option for the Environment filter.")
+        return
+	    
 
 
 
