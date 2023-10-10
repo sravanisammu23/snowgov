6a294e032b3ff57c8a19d2b347532e0fe4eaf7a2
sravanisammu23 <70464115+sravanisammu23@users.noreply.github.com>
2023-10-10 22:46:57 +0530

    Update about.py

--- about.py
+++ about.py
@@ -426,7 +426,8 @@ line-height: normal;
 
     # Display assigned roles in a table without index
     role_df = pd.DataFrame(roles_table_data)
-    st.dataframe(role_df)
+    st.dataframe(role_df, use_container_width=False, width=800)  # Adjust width as needed
+
 
     # Display the roles to revoke multiselect widget
     with col2:
