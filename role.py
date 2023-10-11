4dd29bde23337c4532a7098456df005de6fb1252
sravanisammu23 <70464115+sravanisammu23@users.noreply.github.com>
2023-10-11 10:55:21 +0530

    Update role.py

--- role.py
+++ role.py
@@ -992,12 +992,11 @@ def monitor3(tab_id):
 
     # Subject Area Filter
 
-  with col_proj:
-	    constructed_query = construct_subject_query(selected_environments, selected_projects)
-	    st.write(constructed_query)  # Temporarily print the constructed SQL query
-	    subject_areas = ['All'] + [result[0].strip() for result in execute_query(conn, constructed_query)]
-	    selected_subject_areas = st.multiselect('PROJECT :', subject_areas, default=['All'])
+    with col_proj:
 
+        subject_areas = ['All'] + [result[0].strip() for result in execute_query(conn, construct_subject_query(selected_environments, selected_projects))]
+
+        selected_subject_areas = st.multiselect('PROJECT :', subject_areas, default=['All'])
 
 
 
